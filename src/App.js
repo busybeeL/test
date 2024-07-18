666500cb9e152ef4076a178ac0a951f682f2710a
busybeeL <jiwoooookim@gmail.com>
2024-07-18 12:20:21 +0100

    first pr

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+const title = 'Hello React!';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {title}
         </p>
         <a
           className="App-link"
